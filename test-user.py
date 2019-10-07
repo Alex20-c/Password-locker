1ae315e5afed2ec7e664a48fb6ba939c8c33ace2
Alex Kiprop <kipropalex59@gmail.com>
2019-10-07 07:36:23 +0300

    add atest to test if the user object is saved on the user list

--- test-user.py
+++ test-user.py
@@ -13,4 +13,31 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("Alekilexy","0727200709ak") # create contact object
 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initiated properly
+        '''
+        self.assertEqual(self.new_user.username,"Alekilexy")
+        self.assertEqual(self.new_user.password,"0727200709ak")
+
+    def test_save_user(self):
+        """
+        test_save_user test case to test if the user object is saved into the users' list
+        """
+        self.new_user.save_user()# saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+    def test_user_exists(self):
+        """
+        test_user_exists test case to check if we can return a boolean if we cannot find the user
+        """
+        self.new_user.save_user()
+        test_user = User("User","123456")
+        test_user.save_user()
+        user_exists = User.user_exists("123456")
+        self.assertTrue(user_exists)
+
+if __name__ == '__main__':
+    unittest.main()
+
     
